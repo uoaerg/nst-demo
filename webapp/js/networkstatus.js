992f053d0c98382eb410b55c913e1b5473c08f57
adventureloop <jones@sdf.org>
2016-11-23 22:24:11 +0000

    Start web socket code going

--- webapp/js/networkstatus.js
+++ webapp/js/networkstatus.js
@@ -1,59 +1,84 @@
-var eth1line = c3.generate({
-	bindto: ".eth1-line",
-	data: {
+function setupcharts() 
+{
+	var charts = {}
+	charts.eth1line = c3.generate({
+		bindto: ".eth1-line",
+		data: {
+			columns: [
+				['rx', 30, 200, 100, 400, 150, 250],
+				['tx', 50, 20, 10, 40, 15, 25]
+			]
+		},
+		axis: {
+			y: { show: false },
+			x: { show: false }
+		},
+		legend: { hide: true },
+	});
+
+	charts.eth1donut = c3.generate({
+		bindto: ".eth1-donut",
+		data: {
 		columns: [
-			['rx', 30, 200, 100, 400, 150, 250],
-			['tx', 50, 20, 10, 40, 15, 25]
-		]
-	},
-	axis: {
-		y: { show: false },
-		x: { show: false }
-	},
-	legend: { hide: true },
-});
+			['data1', 30],
+			['data2', 120],
+		],
+		type : 'donut',
+		},
+		donut: {
+			title: "ETH1 RATE"
+		},
+		legend: { hide: true },
+	});
 
-var eth1donut = c3.generate({
-	bindto: ".eth1-donut",
-	data: {
-	columns: [
-		['data1', 30],
-		['data2', 120],
-	],
-	type : 'donut',
-	},
-	donut: {
-		title: "ETH1 RATE"
-	},
-	legend: { hide: true },
-});
+	charts.eth2line = c3.generate({
+		bindto: ".eth2-line",
+		data: {
+			columns: [
+				['rx', 30, 200, 100, 400, 150, 250],
+				['tx', 50, 20, 10, 40, 15, 25]
+			]
+		},
+		axis: {
+			y: { show: false },
+			x: { show: false }
+		},
+		legend: { hide: true },
+	});
 
-var eth2line = c3.generate({
-	bindto: ".eth2-line",
-	data: {
+	charts.eth2donut = c3.generate({
+		bindto: ".eth2-donut",
+		data: {
 		columns: [
-			['rx', 30, 200, 100, 400, 150, 250],
-			['tx', 50, 20, 10, 40, 15, 25]
-		]
-	},
-	axis: {
-		y: { show: false },
-		x: { show: false }
-	},
-	legend: { hide: true },
-});
+			['data1', 30],
+			['data2', 120],
+		],
+		type : 'donut',
+		},
+		donut: {
+			title: "ETH2 RATE"
+		},
+		legend: { hide: true },
+	});
 
-var eth2donut = c3.generate({
-	bindto: ".eth2-donut",
-	data: {
-	columns: [
-		['data1', 30],
-		['data2', 120],
-	],
-	type : 'donut',
-	},
-	donut: {
-		title: "ETH2 RATE"
-	},
-	legend: { hide: true },
-});
+	return charts;
+}
+
+var ws;
+
+function init()
+{
+	console.log("init call");
+	var charts = setupcharts();
+	console.log(charts);
+
+	ws = new WebSocket("ws://localhost:8080/ifstatus", "highspeednetworking");
+	ws.onopen = function (event) {
+		console.log("ws connected");
+		ws.send("Here's some text that the server is urgently awaiting!"); 
+	};
+
+	ws.onmessage = function (event) {
+		console.log(event.data);
+	}
+}
